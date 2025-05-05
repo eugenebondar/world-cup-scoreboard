115cdae4606aee763e3239914243eeb83f0705a9
Dina Cheliadina <dina@Dinas-MacBook-Pro.local>
2025-05-05 22:43:14 +0200

    feat: add update score for Scoreboard

--- src/core/Scoreboard.ts
+++ src/core/Scoreboard.ts
@@ -34,7 +34,7 @@ export class Scoreboard {
             throw new Error('Teams must be different');
         }
 
-        const matchExists = this.matches.some((match) => match.homeTeam === trimmedHomeTeam && match.awayTeam === trimmedAwayTeam);
+        const matchExists = this.matches.some((match: Match) => match.homeTeam === trimmedHomeTeam && match.awayTeam === trimmedAwayTeam);
 
         if (matchExists) {
             throw new Error('Match already started');
@@ -44,5 +44,34 @@ export class Scoreboard {
         this.matches.push(match);
 
         return match;
+    };
+
+    public updateScore(homeTeam: string, awayTeam: string, homeScore: number, awayScore: number) {
+        if (Number.isNaN(homeScore) || Number.isNaN(awayScore)) {
+            throw new Error('Score cannot be NaN');
+        }
+
+        if (homeScore === Infinity || awayScore === Infinity) {
+            throw new Error('Score cannot be Infinity');
+        }
+
+        if (homeScore === -Infinity || awayScore === -Infinity) {
+            throw new Error('Score cannot be -Infinity');
+        }
+
+        if (homeScore < 0 || awayScore < 0) {
+            throw new Error('Score cannot be negative');
+        }
+
+        const trimmedHomeTeam = homeTeam.trim();
+        const trimmedAwayTeam = awayTeam.trim();
+
+        const matchExists = this.matches.find((match: Match) => match.homeTeam === trimmedHomeTeam && match.awayTeam === trimmedAwayTeam);
+
+        if (!matchExists) {
+            throw new Error('Match is not found');
+        }
+
+        matchExists.updateScore(homeScore, awayScore);
     };
 }
