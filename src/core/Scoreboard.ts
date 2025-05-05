19ee9ea97a0278d8610a55ea637fda1e1217d793
Dina Cheliadina <dina@Dinas-MacBook-Pro.local>
2025-05-05 22:11:48 +0200

    feat: add startMatch to Scoreboard

--- src/core/Scoreboard.ts
+++ src/core/Scoreboard.ts
@@ -1,1 +1,29 @@
-export class Scoreboard {}
+import { Match } from './Match';
+
+export class Scoreboard {
+    private matches: Match[] = [];
+
+    public startMatch(homeTeam: string, awayTeam: string): Match {
+        const trimmedHomeTeam = homeTeam.trim();
+        const trimmedAwayTeam = awayTeam.trim();
+
+        if (!trimmedHomeTeam || !trimmedAwayTeam) {
+            throw new Error('Team names are required');
+        }
+
+        if (trimmedHomeTeam === trimmedAwayTeam) {
+            throw new Error('Teams must be different');
+        }
+
+        const matchExists = this.matches.some((match) => match.homeTeam === trimmedHomeTeam && match.awayTeam === trimmedAwayTeam);
+
+        if (matchExists) {
+            throw new Error('Match already started');
+        }
+
+        const match = new Match(trimmedHomeTeam, trimmedAwayTeam);
+        this.matches.push(match);
+
+        return match;
+    };
+}
