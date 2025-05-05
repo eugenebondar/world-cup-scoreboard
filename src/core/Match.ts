54f05fab461be55781eac0be7c53954c7103ebe8
Dina Cheliadina <dina@Dinas-MacBook-Pro.local>
2025-05-05 22:33:23 +0200

    feat: add startTime to Match

--- src/core/Match.ts
+++ src/core/Match.ts
@@ -1,11 +1,14 @@
 export class Match {
     public homeScore: number = 0;
     public awayScore: number = 0;
+    public readonly startTime: number = 0;
 
     constructor(
         public readonly homeTeam: string,
         public readonly awayTeam: string,
-    ) {}
+    ) {
+        this.startTime = Date.now();
+    }
 
     public updateScore(homeScore: number, awayScore: number) {
         this.homeScore = homeScore;
